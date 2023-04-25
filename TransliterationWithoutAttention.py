828aec10ca2d08ed5b7946ad0b938706419aee6a
Pranav335 <98520110+Pranav335@users.noreply.github.com>
2023-04-26 02:51:07 +0530

    Added Training module

--- TransliterationWithoutAttention.py
+++ TransliterationWithoutAttention.py
@@ -109,9 +109,10 @@ class EncoderRNN(nn.Module):
 class DecoderRNN(nn.Module):
     
     def __init__(self, hidden_size, output_size, verbose = False):
-        super.__init__()
+        super(DecoderRNN, self).__init__()
         self.hidden_size = hidden_size
         self.embedding = nn.Embedding(output_size, hidden_size)
+        self.relu = nn.ReLU()
         self.gru = nn.GRU(hidden_size, hidden_size)
         self.out = nn.Linear(hidden_size, output_size)
         self.softmax = nn.LogSoftmax(dim = 1)
@@ -119,7 +120,7 @@ class DecoderRNN(nn.Module):
         
     def forward(self, input, hidden):
         output = self.embedding(input).view(1, 1, -1)
-        output = nn.relu(output)
+        output = self.relu(output)
         output, hidden = self.gru(output, hidden)
         output = self.softmax(self.out(output[0]))
         if self.verbose == True:
@@ -132,18 +133,98 @@ class DecoderRNN(nn.Module):
         return torch.zeros(1, 1, self.hidden_size)
     
     
-encoder = EncoderRNN(english.n_letters, 120, verbose = True)
-hidden = encoder.initHidden()
-#encoder(df1["English Representation"][1], hidden)
-embedding = nn.Embedding(hindi.n_letters, 120)
-decoder_input = torch.tensor([[SOS_token]])
-output = embedding(decoder_input)
-enc_output, hidden = encoder(df1["English Representation"][1], hidden)
-gru = nn.GRU(120, 120)
-dec_out, hidden = gru(output, hidden)
-linear = nn.Linear(120, hindi.n_letters)
-dec_out = linear(dec_out)
-softmax = nn.LogSoftmax(dim = 1)
-softmax(dec_out)
-topv, topi = dec_out.topk(1)
-decoder_input = topi.squeeze().detach()
+# encoder = EncoderRNN(english.n_letters, 120, verbose = True)
+# hidden = encoder.initHidden()
+# #encoder(df1["English Representation"][1], hidden)
+# embedding = nn.Embedding(hindi.n_letters, 120)
+# decoder_input = torch.tensor([[SOS_token]])
+# output = embedding(decoder_input)
+# enc_output, hidden = encoder(df1["English Representation"][1], hidden)
+# gru = nn.GRU(120, 120)
+# dec_out, hidden = gru(output, hidden)
+# linear = nn.Linear(120, hindi.n_letters)
+# dec_out = linear(dec_out)
+# softmax = nn.LogSoftmax(dim = 1)
+# softmax(dec_out)
+# topv, topi = dec_out.topk(1)
+# decoder_input = topi.squeeze().detach()
+
+import torch.optim as optim
+
+def train(input_tensor, target_tensor, encoder, decoder,
+             encoder_optimizer, decoder_optimizer, criterion):
+    
+    encoder_hidden = encoder.initHidden()
+    
+    loss = 0
+    
+    encoder_optimizer.zero_grad()
+    decoder_optimizer.zero_grad()
+    
+    #input_length = input_tensor.size(0) # L x 1 x features
+    target_length = target_tensor.size(0) # L x 1 x features
+    
+    encoder_output, encoder_hidden = encoder(input_tensor, encoder_hidden)
+    
+    decoder_input = torch.tensor([SOS_token])
+    
+    decoder_hidden = encoder_hidden
+    
+    for i in range(target_length):
+        decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden)
+        topv, topi = decoder_output.topk(1)
+        decoder_input = topi.squeeze().detach()
+        # print('Decoder output shape',decoder_output.shape)
+        # print('target_tensor shape', target_tensor[i].view(1).shape)
+        # print('target_tensor', target_tensor[i])
+        loss += criterion(decoder_output, target_tensor[i].view(1))
+        if decoder_input.item() == EOS_token:
+            break
+    
+    loss.backward()
+    
+    encoder_optimizer.step()
+    decoder_optimizer.step()
+    
+    return loss.item()/target_length
+
+import matplotlib.pyplot as plt
+
+def trainiter(encoder, decoder, n_iters, print_every = 1000, plot_every = 100,
+              learning_rate = 0.01):
+    
+    plot_losses = []
+    print_loss_total = 0  # Reset every print_every
+    plot_loss_total = 0  # Reset every plot_every
+    
+    encoder_optimizer = optim.SGD(encoder.parameters(), lr = learning_rate)
+    decoder_optimizer = optim.SGD(decoder.parameters(), lr = learning_rate)
+    criterion = nn.NLLLoss()
+    for i in range(n_iters):
+        input_tensor, target_tensor = df1.iloc[i, 2:].values
+        loss = train(input_tensor, target_tensor, encoder, decoder,
+                     encoder_optimizer, decoder_optimizer, criterion)
+        
+        print_loss_total += loss
+        plot_loss_total += loss
+        
+        if i % print_every == 0:
+            print_loss_avg = print_loss_total/ print_every
+            print_loss_total = 0
+            print(f'Loss at {i}: {print_loss_avg}')
+            
+        if i % plot_every == 0:
+            plot_loss_avg = plot_loss_total/plot_every
+            plot_losses.append(plot_loss_avg)
+            plot_loss_total = 0
+    
+    plt.plot(plot_losses)
+    
+    
+    
+encoder = EncoderRNN(english.n_letters, 16)
+decoder = DecoderRNN(16, hindi.n_letters)
+
+n = df1.shape[0]
+
+trainiter(encoder, decoder, n)
